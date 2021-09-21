c7cba02f6e9385809b80007fee38de6dc2c998cb
EOfori <73847282+EOfori@users.noreply.github.com>
2021-09-21 13:51:40 -0500

    Update R Code.R

--- R Code.R
+++ R Code.R
@@ -1,30 +1,21 @@
 ## Spatial autocorrelation test for 2017 Kansas county level ag land values
 
 library(tidyverse)
-mydata <- as.data.frame(read_csv("2017_KS_AgLandValues.csv")) # read in ag land value data
-
 library(tigris)
+mydata <- as.data.frame(read_csv("2017_KS_AgLandValues.csv")) # read in ag land value data
 kansas <- counties(state = "KS",  cb = TRUE) # read in kansas shapefile using "tigris" package
-
 ks_df <- geo_join(kansas, mydata, "NAME", "County", how="left") # Merging data using geo_join
-
 
 ## Data visualization ####
 library(tmap)
 tm_shape(ks_df) + tm_polygons(style="quantile", col = "AgLandValue") +
   tm_legend(outside = TRUE, text.size = .8) 
 
-
 ## Moran's I test on Ag land values  ####     
 library(spdep)
 nb <- poly2nb(ks_df, queen=TRUE) # construct neighbours based on counties
-
 lw <- nb2listw(nb, style="W", zero.policy=TRUE) # create weight matrix
-
 AgLandValue.lag <- lag.listw(lw, ks_df$AgLandValue) # create spatially lagged values
-
 moran.test(ks_df$AgLandValue,lw) # moran's I test
-
 MC <- moran.mc(ks_df$AgLandValue, lw, nsim=500); MC  # moran's I test based on Monte Carlo simulated method
-
 plot(MC, main="", las=1) # Plot the distribution 
